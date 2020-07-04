cf4df34219cfc7244031bd86ecf373407410db61
Igor Malyarov <igor1309@gmail.com>
2020-07-04 11:51:32 +0300

    Initial Commit

---
+++ InvestmentTracker/InvestmentTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvestmentTrackerApp.swift
+//  InvestmentTracker
+//
+//  Created by Igor Malyarov on 04.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct InvestmentTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
